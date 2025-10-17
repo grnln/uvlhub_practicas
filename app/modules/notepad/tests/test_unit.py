a26a66abc6fc16d71acdfbf7fe063a0a4c2dac56
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2025-10-17 19:45:33 +0200

    Added unit tests for the notepad module for practice session 4.

--- app/modules/notepad/tests/test_unit.py
+++ app/modules/notepad/tests/test_unit.py
@@ -1,5 +1,8 @@
 import pytest
 
+from app import db
+from app.modules.auth.models import User
+from app.modules.conftest import login, logout
 
 @pytest.fixture(scope='module')
 def test_client(test_client):
@@ -7,12 +10,10 @@ def test_client(test_client):
     Extends the test_client fixture to add additional specific data for module testing.
     """
     with test_client.application.app_context():
-        # Add HERE new elements to the database that you want to exist in the test context.
-        # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
-        pass
-
+        user_test = User(email = "user@example.com", password = "test1234")
+        db.session.add(user_test)
+        db.session.commit()
     yield test_client
-
 
 def test_sample_assertion(test_client):
     """
@@ -22,3 +23,99 @@ def test_sample_assertion(test_client):
     """
     greeting = "Hello, World!"
     assert greeting == "Hello, World!", "The greeting does not coincide with 'Hello, World!'"
+
+def test_list_empty_notepad_get(test_client):
+    """
+    Tests access to the empty notepad list via GET request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.get("/notepad")
+    assert response.status_code == 200, "The notepad page could not be accessed."
+    assert b"You have no notepads." in response.data, "The expected content is not present on the page."
+
+    logout(test_client)
+
+def test_get_notepad_create(test_client):
+    """
+    Tests accessibility of the notepad creation screen via GET request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.get("/notepad/create")
+    assert response.status_code == 200, "The notepad creation page could not be accessed."
+    assert b"Title" in response.data, "The expected content is not present on the page."
+    assert b"Body" in response.data, "The expected content is not present on the page."
+
+    logout(test_client)
+
+def test_notepad_create(test_client):
+    """
+    Tests the notepad creation functionality via POST request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.post("/notepad/create", data = {"title": "sample title", "body": "sample body"})
+    assert response.status_code == 302, "Could not create a sample notepad."
+
+    response = test_client.get("/notepad")
+    assert response.status_code == 200, "The notepad page could not be accessed."
+    assert b"sample title" in response.data, "The expected content is not present on the page."
+    assert b"sample body" in response.data, "The expected content is not present on the page."
+
+    logout(test_client)
+
+def test_get_notepad_edit(test_client):
+    """
+    Tests accessibility of the notepad edition functionality via GET request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.post("/notepad/create", data = {"title": "sample title", "body": "sample body"})
+    assert response.status_code == 302, "Could not create a sample notepad."
+
+    response = test_client.get("/notepad/edit/1")
+    assert response.status_code == 200, "The notepad edition page could not be accessed."
+    assert b"Title" in response.data, "The expected content is not present on the page."
+    assert b"Body" in response.data, "The expected content is not present on the page."
+
+    logout(test_client)
+
+def test_notepad_edit(test_client):
+    """
+    Tests the notepad edition functionality via POST request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.post("/notepad/create", data = {"title": "sample title", "body": "sample body"})
+    assert response.status_code == 302, "Could not create a sample notepad."
+
+    response = test_client.post("/notepad/edit/1", data = {"title": "sample title edited", "body": "sample body edited"})
+    assert response.status_code == 302, "Could not edit sample notepad."
+
+    response = test_client.get("/notepad")
+    assert response.status_code == 200, "The notepad page could not be accessed."
+    assert b"sample title edited" in response.data, "The expected content is not present on the page."
+    assert b"sample body" in response.data, "The expected content is not present on the page."
+
+    logout(test_client)
+
+def test_notepad_delete(test_client):
+    """
+    Tests the notepad deletion functionality via POST request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.post("/notepad/create", data = {"title": "sample title", "body": "sample body"})
+    assert response.status_code == 302, "Could not create a sample notepad."
+
+    response = test_client.post("/notepad/delete/1")
+    assert response.status_code == 302, "Could not delete sample notepad."
+
+    logout(test_client)
